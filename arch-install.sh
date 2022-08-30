0728dc370053e135b146a754b8effe286f4d15c1
WhiteH4T-Dev <83751620+WhiteH4T-Dev@users.noreply.github.com>
2022-08-31 04:03:24 +1000

    Update arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -9,7 +9,7 @@ timedatectl status
 #Partition Disks
 fdisk -l
 fdisk /dev/sda
-(echo m; echo g; echo m; echo n; echo "";  echo ""; echo +3G; echo n; echo 2; echo ""; echo +3G; echo n; echo 3; echo ""; echo ""; echo m; echo t; echo 1; echo 1; echo t; echo 2; echo 19; echo m; echo w;) | fdisk /dev/sda
+(echo "m"; echo "g"; echo "m"; echo "n"; echo "";  echo ""; echo "+3G"; echo "n"; echo "2"; echo ""; echo "+3G"; echo "n"; echo "3"; echo ""; echo ""; echo "m"; echo "t"; echo "1"; echo "1"; echo "t"; echo "2"; echo "19"; echo "m"; echo "w") | fdisk /dev/sda
 clear
 #Format 
 mkfs.fat -F32 /dev/sda1
